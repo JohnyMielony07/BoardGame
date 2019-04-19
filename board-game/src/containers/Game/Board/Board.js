989e9325444dbee65ca5f5832510c3d2367f2156
JohnyMielony07 <jan.latoch@gmail.com>
2019-04-19 22:20:47 +0200

    add state of special fields

--- board-game/src/containers/Game/Board/Board.js
+++ board-game/src/containers/Game/Board/Board.js
@@ -1,10 +1,40 @@
 import React, {Component} from 'react';
 
 class Board extends Component {
+
+    state = {
+        fieldsNumber: 20,
+        playerPosition: [1],
+        winField: [20],
+        gameOverField: [11],
+        fields: []
+    }
+
     render() {
+        let fields = [];
+        let outterFields = [];
+        for(let i = 0; i < this.state.fieldsNumber; i++) {
+             fields.push('normal');                          
+        }
+
+        for(let i = 0; i < this.state.winField.length; i++) {
+            fields[this.state.winField[i] - 1] ='win';
+        }
+
+        for(let i = 0; i < this.state.gameOverField.length; i++) {
+            fields[this.state.gameOverField[i] - 1] ='gameOver';
+        }
+
+        fields.map(el => {
+            
+        })
+
+        console.log(fields);
+
         return (
             <div>
                 Board
+
             </div>
         )
     }
