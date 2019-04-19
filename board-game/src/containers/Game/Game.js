ce602ccbce80727976b156068a3f0d5d641007f9
JohnyMielony07 <jan.latoch@gmail.com>
2019-04-19 21:29:25 +0200

    add Game component

---
+++ board-game/src/containers/Game/Game.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Game extends Component {
+    render() {
+        return (
+            <div>
+                Game Setion
+            </div>
+        )
+    }
+}
+
+export default Game;
