342e3641c0c49d1957cb4222018528abc94736c6
JohnyMielony07 <jan.latoch@gmail.com>
2019-04-19 22:25:16 +0200

    add field component

---
+++ board-game/src/components/Field/Field.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Field = (props) => {
+    var name;
+
+    switch(props.type) {
+        case normal:
+            name = '';
+            break;
+        case win:
+            name = 'Finish';
+            break;
+        case gameOver:
+            name = 'Be careful';
+            break;
+    }
+    return (
+        <div>
+            {name}
+        </div>
+    )
+}
+
+export default Field;
